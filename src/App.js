72b85814494c7099d58ef4a478d4bb00910157d8
Monde <monde@nomanini.com>
2019-06-02 15:25:27 +0200

    Style app components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 import React from "react";
 import "./App.scss";
 
+import CurrencySymbol from "./components/CurrencySymbol/index";
+
 function App() {
   return (
     <div className="l-app">
       <article className="l-app_col--first">
         <div className="c-cal">
-          <div className="c-cal_display" />
-          <div className="c-cal_display" />
+          <div className="c-cal_display">
+            <CurrencySymbol />
+            <div className="c-display_value">0.00</div>
+          </div>
+
+          <div className="c-cal_keypad">
+            <div className="c-keypad_digit c-keypad_digit--1">1</div>
+            <div className="c-keypad_digit c-keypad_digit--2">2</div>
+            <div className="c-keypad_digit c-keypad_digit--3">3</div>
+            <div className="c-keypad_digit c-keypad_digit--4">4</div>
+            <div className="c-keypad_digit c-keypad_digit--5">5</div>
+            <div className="c-keypad_digit c-keypad_digit--6">6</div>
+            <div className="c-keypad_digit c-keypad_digit--7">7</div>
+            <div className="c-keypad_digit c-keypad_digit--8">8</div>
+            <div className="c-keypad_digit c-keypad_digit--9">9</div>
+            <div className="c-keypad_digit c-keypad_digit--cancel">❌</div>
+            <div className="c-keypad_digit c-keypad_digit--0">0</div>
+            <div className="c-keypad_digit c-keypad_digit--accept">✅</div>
+          </div>
         </div>
       </article>
 
-      <div className="l-app_col--last">
-        <div className="c-charge-list-container">
-          <div className="c-charge-list">
-            <ul>
-              <li>
-                <span>R1.23</span>
-                <span>R4.56</span>
-                <span>R7.89</span>
-              </li>
-            </ul>
-          </div>
+      <div className="l-app_col--last c-charge-list">
+        <div className="c-charge-section_container">
+          <ul className="c-charge-section_list">
+            <li>
+              <CurrencySymbol />
+              <span>1.23</span>
+            </li>
+            <li>
+              <CurrencySymbol />
+              <span>4.56</span>
+            </li>
+            <li>
+              <CurrencySymbol />
+              <span>7.89</span>
+            </li>
+          </ul>
 
-          <hr className="c-charge_divider" />
+          <div className="c-charge-section_divider" />
 
-          <div className="c-charge-total">
-            <span className="currency-symbol">R</span>
-            <span className="amount">13.68</span>
+          <div className="c-charge-section_total">
+            <CurrencySymbol />
+            <span className="a-amount">13.68</span>
           </div>
         </div>
       </div>
---
+++ src/components/CurrencySymbol/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./style.scss";
+
+export default function CurrencySymbol() {
+  // TODO: Use browser intl api to get real currency code
+  return <span className="a-currency-symbol">R</span>;
+}
