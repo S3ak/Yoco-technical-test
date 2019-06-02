04aae2e0f83fd31b3348c12d9b71c4048f3a8c4c
Monde <monde@nomanini.com>
2019-06-02 15:38:25 +0200

    Get total for entries

--- src/App.js
+++ src/App.js
@@ -3,18 +3,47 @@ import "./App.scss";
 
 import CurrencySymbol from "./components/CurrencySymbol/index";
 
+const { useState, useEffect } = React;
+
+const mockEntries = [1.23, 4.56, 7.89];
+
+// TODO: needs to be an array of Entry objects
+// const mockEntry = {
+//   id: 1,
+//   val: 1.23,
+// }
+
+function getSum(total, num) {
+  return total + Math.round(num);
+}
+
 function App() {
+  const [currentVal, setCurrentVal] = useState(0.0);
+  const [chargeEntries, setChargeEntries] = useState(mockEntries);
+  const [totalEntriesVal, setTotalEntriesVal] = useState();
+
+  useEffect(() => {
+    if (chargeEntries) {
+      setTotalEntriesVal(chargeEntries.reduce(getSum, 0));
+    }
+  }, [chargeEntries]);
+
   return (
     <div className="l-app">
       <article className="l-app_col--first">
         <div className="c-cal">
           <div className="c-cal_display">
             <CurrencySymbol />
-            <div className="c-display_value">0.00</div>
+            <div className="c-display_value">{currentVal}</div>
           </div>
 
           <div className="c-cal_keypad">
-            <div className="c-keypad_digit c-keypad_digit--1">1</div>
+            <div
+              onClick={e => setCurrentVal(val => val + 1)}
+              className="c-keypad_digit c-keypad_digit--1"
+            >
+              1
+            </div>
             <div className="c-keypad_digit c-keypad_digit--2">2</div>
             <div className="c-keypad_digit c-keypad_digit--3">3</div>
             <div className="c-keypad_digit c-keypad_digit--4">4</div>
@@ -33,25 +62,19 @@ function App() {
       <div className="l-app_col--last c-charge-list">
         <div className="c-charge-section_container">
           <ul className="c-charge-section_list">
-            <li>
-              <CurrencySymbol />
-              <span>1.23</span>
-            </li>
-            <li>
-              <CurrencySymbol />
-              <span>4.56</span>
-            </li>
-            <li>
-              <CurrencySymbol />
-              <span>7.89</span>
-            </li>
+            {chargeEntries.map((c, idx) => (
+              <li key={`${idx}-${Math.random()}`}>
+                <CurrencySymbol />
+                <span>{c}</span>
+              </li>
+            ))}
           </ul>
 
           <div className="c-charge-section_divider" />
 
           <div className="c-charge-section_total">
             <CurrencySymbol />
-            <span className="a-amount">13.68</span>
+            <span className="a-amount">{totalEntriesVal}</span>
           </div>
         </div>
       </div>
