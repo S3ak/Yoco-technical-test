4612d114360400f7bdd0e92d1f69952339baea04
Monde <monde@nomanini.com>
2019-06-02 14:38:08 +0200

    Scaffold out skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="l-app">
+      <article className="l-app_col--first">
+        <div className="c-cal">
+          <div className="c-cal_display" />
+          <div className="c-cal_display" />
+        </div>
+      </article>
+
+      <div className="l-app_col--last">
+        <div className="c-charge-list-container">
+          <div className="c-charge-list">
+            <ul>
+              <li>
+                <span>R1.23</span>
+                <span>R4.56</span>
+                <span>R7.89</span>
+              </li>
+            </ul>
+          </div>
+
+          <hr className="c-charge_divider" />
+
+          <div className="c-charge-total">
+            <span className="currency-symbol">R</span>
+            <span className="amount">13.68</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
